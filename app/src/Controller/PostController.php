6e3cedc5cdd08dcf4022ff1d8003b8518ee8255d
Weronika <you@example.com>
2023-05-29 08:41:22 +0200

    sensio and paginator

--- app/src/Controller/PostController.php
+++ app/src/Controller/PostController.php
@@ -11,8 +11,6 @@
  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  use Symfony\Component\HttpFoundation\Response;
  use Symfony\Component\Routing\Annotation\Route;
- use App\Controller\Request;
- 
 
 /**
  * Class PostController.
